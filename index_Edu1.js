5c58b7c11ae42fdc6a241342f590c566bea2f34a
npranali512 <npranali512@gmail.com>
2020-04-25 21:50:40 +0530

    Commited on 25/04/2020 by Pranali i.e index file only first commit in local repo

---
+++ index_Edu1.js
@@ -0,0 +1,11 @@
+// First File Edu 1
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
+
+const App = () => {
+    return (
+        <Text>Edureka</Text>
+    );
+};
+
+AppRegistry.registerComponent('Edureka', () => App);
